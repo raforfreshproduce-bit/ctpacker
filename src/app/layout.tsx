b11301daf7de48fc0ccacce1077749cd84ba6ee5
ericnangomba <ericnangomba@hotmail.com>
2025-11-24 13:23:24 +0200

    Revert "fixed again"
    This reverts commit 7a5042ea998f0f677299eddae453e41f3b060389.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'VWSA CPT WAREHOUSE | CTPacker Tracker',
+  title: 'CTPacker Tracker',
   description: 'Real-Time Packer/Picker Assignment Registry',
 };
 
